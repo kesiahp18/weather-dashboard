931c2c5c2b533967c7d7f217215d2f511cac6bef
kesiahp18 <kesiahp18@gmail.com>
2021-04-12 11:05:38 -0400

    get basic weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,30 @@ const searchButtonEl = document.getElementById("searchButton");
 searchButtonEl.addEventListener("click", function (event) {
     event.preventDefault();
 
-    var city = document.querySelector("#city-search").value.trim();
+    const city = document.querySelector("#city-search").value.trim();
     console.log(city);
 
+    displayWeather(city);
 });
+
+function displayWeather(city) {
+    const todaysWeather = document.getElementById("weather-report");
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+      )
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        todaysWeather.innerHTML = `
+          <h3>${data.name} (${date})</h3>
+          <p>High: ${data.main.temp_max} F </p>
+          <p>Low: ${data.main.temp_min} F </p>
+          <p>Humidity: ${data.main.humidity}%</p>
+          <p>Wind: ${data.wind.speed} mph</p>
+          
+         `;
+        console.log(data);
+   
+    });
+}
